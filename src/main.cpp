bb77bafefbd4a91face7823012ac5addff4992d7
HeronErin <HeronErin@protonmail.com>
2024-01-18 14:01:25 -0500

    glp

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,18 @@
 #include  <time.h>
 using namespace vex;
 
+
+// Copyright (C) 2024 HeronErin
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation; either version 3 of the
+// License, or (at your option) any later version.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 // A global instance of vex::brain used for printing to the V5 brain screen
 vex::brain       Brain;
 
